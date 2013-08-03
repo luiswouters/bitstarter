a16c87bca81b49d9ae89683c3c82441b0d8bfa9b
Luis Wouters <luiswouters@gmail.com>
2013-08-03 21:00:19 +0000

    Changes of the homework 4 and some of the 3.

--- grader.js
+++ grader.js
@@ -24,15 +24,18 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
+var url = require('url');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLHTMLCODE = '';
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
+    }    
     return instr;
 };
 
@@ -54,6 +57,16 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
+var checkHtmlUrl = function(checksfile) {
+    $ = cheerio.load(URLHTMLCODE);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -61,14 +74,35 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var start = function(){
+    if(typeof(program.url) !== 'undefined'){
+	var checkJson = checkHtmlUrl(program.checks);
+	//var checkJson = checkHtmlFile(program.file, program.checks);
+    } else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+    }
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        .option('-u, --url <url_file>', 'Path to url content') 
+	.parse(process.argv);
+    if(typeof(program.url) !== 'undefined'){
+	restler.get(program.url).on('complete', function(content,response){
+	    if(response == null){
+		console.log("%s does not exist. Exiting.", program.url);
+	    } else {
+		URLHTMLCODE = content;
+		start();
+	    }
+	});
+    } else {
+	start();
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
